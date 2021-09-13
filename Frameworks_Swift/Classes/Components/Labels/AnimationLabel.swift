34ed1fecdcf2333892b1db5b9bda45164493c99e
Jerry <lhp3851@163.com>
2021-09-13 17:15:32 +0800

    [feature][模块化]：1. 子模块化组件；

---
+++ Frameworks_Swift/Classes/Components/Labels/AnimationLabel.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationLabel.swift
+//  Alamofire
+//
+//  Created by sumian on 2021/9/13.
+//
+
+import UIKit
+
+class AnimationLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Frameworks_Swift/Classes/Debug/Tools.swift
@@ -0,0 +1,12 @@
+//
+//  Tools.swift
+//  Alamofire
+//
+//  Created by sumian on 2021/9/13.
+//
+
+import UIKit
+
+class Tools: NSObject {
+
+}
